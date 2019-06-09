da7d1af93dcf0eeba74b6f5ec8e76195d4c686a2
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2019-06-09 18:32:31 +0900

    bunlde gem

---
+++ lib/itamae/plugin/resource/aur_package.rb
@@ -0,0 +1,12 @@
+require "itamae/plugin/resource/aur_package/version"
+
+module Itamae
+  module Plugin
+    module Resource
+      module AurPackage
+        class Error < StandardError; end
+        # Your code goes here...
+      end
+    end
+  end
+end
