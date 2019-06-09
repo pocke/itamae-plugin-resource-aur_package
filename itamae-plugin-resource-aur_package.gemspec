c72c1516a4cf9dcb13f93a76a38d6bcf6afc038d
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2019-06-09 19:23:55 +0900

    Inital implementation

--- itamae-plugin-resource-aur_package.gemspec
+++ itamae-plugin-resource-aur_package.gemspec
@@ -1,22 +1,21 @@
 lib = File.expand_path("lib", __dir__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
-require "itamae/plugin/resource/aur_package/version"
 
 Gem::Specification.new do |spec|
   spec.name          = "itamae-plugin-resource-aur_package"
-  spec.version       = Itamae::Plugin::Resource::AurPackage::VERSION
+  spec.version       = '0.1.0'
   spec.authors       = ["Masataka Pocke Kuwabara"]
   spec.email         = ["kuwabara@pocke.me"]
 
-  spec.summary       = %q{TODO: Write a short summary, because RubyGems requires one.}
-  spec.description   = %q{TODO: Write a longer description or delete this line.}
-  spec.homepage      = "TODO: Put your gem's website or public repo URL here."
+  spec.summary       = %q{An Itamae resource for AUR}
+  spec.description   = %q{An Itamae resource for AUR (Arch User Repository)}
+  spec.homepage      = "https://github.com/pocke/itamae-plugin-resource-aur_package"
 
-  spec.metadata["allowed_push_host"] = "TODO: Set to 'http://mygemserver.com'"
+  # spec.metadata["allowed_push_host"] = "TODO: Set to 'http://mygemserver.com'"
 
   spec.metadata["homepage_uri"] = spec.homepage
-  spec.metadata["source_code_uri"] = "TODO: Put your gem's public repo URL here."
-  spec.metadata["changelog_uri"] = "TODO: Put your gem's CHANGELOG.md URL here."
+  spec.metadata["source_code_uri"] = spec.homepage
+  # spec.metadata["changelog_uri"] = "TODO: Put your gem's CHANGELOG.md URL here."
 
   # Specify which files should be added to the gem when it is released.
   # The `git ls-files -z` loads the files in the RubyGem that have been added into git.
@@ -27,6 +26,7 @@ Gem::Specification.new do |spec|
   spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
   spec.require_paths = ["lib"]
 
+  spec.add_runtime_dependency "itamae"
   spec.add_development_dependency "bundler", "~> 2.1.a"
   spec.add_development_dependency "rake", "~> 12.0"
 end
--- lib/itamae/plugin/resource/aur_package.rb
+++ lib/itamae/plugin/resource/aur_package.rb
@@ -1,11 +1,77 @@
-require "itamae/plugin/resource/aur_package/version"
+require 'itamae'
 
 module Itamae
   module Plugin
     module Resource
-      module AurPackage
-        class Error < StandardError; end
-        # Your code goes here...
+      class AurPackage < ::Itamae::Resource::Base
+        class NotSupportedError < StandardError
+          def initialize(os)
+            @os = os
+          end
+
+          def message
+            "AurPackage only supports Arch Linux, but it is #{@os[:family]}"
+          end
+        end
+
+        define_attribute :action, default: :install
+        define_attribute :name, type: String, default_name: true
+        define_attribute :options, type: String
+        define_attribute :version, type: String
+
+        def run
+          os = backend.instance_variable_get(:@backend).os_info
+          raise NotSupportedError.new(os) unless os[:family] == 'arch'
+
+          super
+        end
+
+        def pre_action
+          case @current_action
+          when :install
+            attributes.installed = true
+          when :remove
+            attributes.installed = false
+          end
+        end
+
+        def set_current_attributes
+          current.installed = run_specinfra(:check_package_is_installed, attributes.name)
+
+          if current.installed
+            current.version = run_specinfra(:get_package_version, attributes.name).stdout.strip
+          end
+        end
+
+        def action_install(action_options)
+          if run_specinfra(:check_package_is_installed, attributes.name, attributes.version)
+            return
+          end
+
+          yay_executable = run_command('which yay').exit_status == 0
+          if yay_executable
+            run_command("yay -S --noconfirm #{attributes.options} #{attributes.name}")
+          else
+            name = attributes.name
+            tmp_dir = "/tmp/itamae-plugin-resource-aur_package-#{name}-#{Time.now.to_f}"
+            run_command(<<~SHELL)
+              mkdir #{tmp_dir} &&
+              cd #{tmp_dir} &&
+              curl -L -O https://aur.archlinux.org/cgit/aur.git/snapshot/#{name}.tar.gz &&
+              tar -xvf #{name}.tar.gz &&
+              cd #{name} &&
+              makepkg -si --noconfirm #{attributes.options}
+            SHELL
+          end
+          updated!
+        end
+
+        def action_remove(action_options)
+          if run_specinfra(:check_package_is_installed, attributes.name, nil)
+            run_specinfra(:remove_package, attributes.name, attributes.options)
+            updated!
+          end
+        end
       end
     end
   end
